21961d568e122b4e55e7ae31e7ef782d60e1ff0c
diazblanco <diazblancodesign@gmail.com>
2022-03-23 19:34:21 +0100

    PHP i Objects - Nivell 1 - Correcció exercici 1

---
+++ Nivell1/ex1/Employee.php
@@ -0,0 +1,35 @@
+<?php
+class Employee {
+    private $nom;
+    private $sou;
+
+public function __construct ($nom, $sou){
+    $this->nom = $nom;
+    $this->sou = $sou;
+}
+/* public function initialize ($nom, $sou){
+    $this->nom = $nom;
+    $this->sou = $sou;
+} */
+
+public function print(){
+    if ($this->isSalaryGreaterThan6000()){
+        //echo $this->nom;
+        echo "$this->nom ha de pagar impostos";
+    } else { 
+        //echo $this->nom;
+        echo "$this->nom no ha de pagar impostos";
+    }
+}
+
+public function isSalaryGreaterThan6000(){
+    $isGreater = false;
+    if ($this->sou > 6000){
+        $isGreater = true;
+    }
+    return $isGreater;
+}
+
+}
+
+?>
---
+++ Nivell1/ex1/index.php
@@ -0,0 +1,11 @@
+<?php
+
+include 'Employee.php';
+
+$employee1 = new Employee("Irene",6000);
+$employee2 = new Employee("Marc",6001);
+
+$employee1->print();
+$employee2->print();
+
+?>
